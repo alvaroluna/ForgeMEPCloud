4124c814d5c2ebb2597475ac8ae8547274bfe1f5
Alvaro Luna <alvaro.luna@autodesk.com>
2020-09-03 18:16:47 -0700

    Added style.css file and linked it to index.html

--- start.js
+++ start.js
@@ -2,6 +2,11 @@
 // PROGRAM ENTRY POINT //
 // =================== //
 
+//#region Notes
+// To start, maneuver to directory with start.js
+// In console type 'node start.js'
+//#endregion
+
 // import required packages
 var express = require("express"); // for web server
 var axios = require("axios"); // a promised base http client (api calls)
@@ -10,7 +15,7 @@ var bodyParser = require("body-parser"); // receive json data
 // setup express web server
 var app = express();
 app.use(bodyParser.json());
-app.use(express.static(__dirname + "/assets")); // this is unfamiliar look into it
+app.use(express.static(__dirname + "/assets/html")); // location of html files...this is unfamiliar look into it
 
 // setup server to listen to port 3000
 app.set("port", 3000);
